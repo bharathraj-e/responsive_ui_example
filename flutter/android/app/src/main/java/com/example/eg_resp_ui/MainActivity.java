77d102fbe733376df05dff642351b9f9ef429f77
Bharathraj <ebraj1996@gmail.com>
2020-06-15 17:40:35 +0530

    v2.0.1

--- flutter/android/app/src/main/java/com/example/eg_resp_ui/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.eg_resp_ui;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- flutter/lib/responsive_ui/responsive_ui.dart
+++
@@ -1,4 +0,0 @@
-library responsive_ui;
-
-export './src/_div.dart';
-export './src/_responsive.dart';
